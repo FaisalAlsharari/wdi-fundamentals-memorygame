f62ea58b760cb0efed5eeb2fd0cbd16883807a2b
faisaltechcampus <48607438+faisaltechcampus@users.noreply.github.com>
2019-05-28 14:00:18 +0300

    You think you have the STRONGEST MEMORY!? 4

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,20 +3,59 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+
+             {
+               rank: 'queen',
+               suit: 'hearts',
+               cardImage: 'images/queen-of-hearts.png'
+              },
+             {
+              rank: 'queen',
+              suit: 'diamonds',
+              cardImage: 'images/queen-of-diamonds.png'
+             },
+             {
+              rank: 'king',
+               suit: 'hearts',
+               cardImage: 'images/king-of-hearts.png'
+             },
+             {
+             rank: 'king',
+               suit: 'diamonds',
+               cardImage: 'images/king-of-diamonds.png'
+             }
+
+            ];
 var cardsInPlay = [];
 function checkForMatch(){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
   console.log("You found a match!");
 } else {
-  console.log("Sorry, try again.");
+ 
+ alert("Sorry, try again.");
 }
 }
 function flipCard(cardId){
 if(cardsInPlay.length === 2)
           checkForMatch()
-      console.log("User flipped" + " " + cards[cardId]);
-      cardsInPlay.push(cards[cardId]);
+      console.log("User flipped" + " " + cards[cardId].rank);
+      cardsInPlay.push(cards[cardId].rank);
+      if (cardsInPlay.length >1){
+      	checkForMatch();
+
+      }
+
+      //jasjahsasjas
+       console.log(cards[cardId].suit);
+      cardsInPlay.push(cards[cardId].suit);
+      if (cardsInPlay.length >1){
+      	checkForMatch();
+
+      }
+
+      console.log(cards[cardId].cardImage);
+      cardsInPlay.push(cards[cardId].cardImage);
       if (cardsInPlay.length >1){
       	checkForMatch();
 
